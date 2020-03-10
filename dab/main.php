a87da823f14d3441b90c4f45f3ee1011072d2045
Flavien Rodrigues <flavien.rodrigues@tiime.fr>
2020-03-10 09:15:30 +0100

    add radio

--- dab/main.php
+++ dab/main.php
@@ -9,17 +9,29 @@ require_once('vendor/autoload.php');
 
 $climate = new League\CLImate\CLImate;
 
-$climate->out('==DAB==');
+$climate->green()->out('==DAB==');
 
 // 1. identifier l'utilisateur
 $lecteurCarte = new LecteurCarte(new CompteRepository());
 $utilisateur = $lecteurCarte->getUtilisateur();
 
-$climate->out("Bonjour " . $utilisateur->getNom());
+$climate->br()->out("Bonjour " . $utilisateur->getNom());
 
-$input = $climate->input('entrez votre PIN');
-$codePIN = $input->prompt();
+//$input = $climate->br()->password('entrez votre PIN : ');
+//$codePIN = $input->prompt();
 
 // 1.1 est-ce que le PIN est ok ?
 
 // 2. proposer les actions possibles
+$options  = [
+    'retrait' => "Retirer de l'argent"
+];
+
+if ($utilisateur instanceof Client) {
+    $options['depot'] = "Déposer de l'argent";
+    $options['consultation'] = "Consulter la balance du compte";
+}
+
+$input    = $climate->br()->radio('Que souhaitez vous faire :', $options);
+$response = $input->prompt();
+var_dump($response);
